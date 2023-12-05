716870dcb93ac1d2035825184fdde78fb7a01d3c
RomanKosov <kosovr48@gmail.com>
2023-12-05 22:45:55 +0500

    add Method

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,19 +1,25 @@
 ﻿//Задайте двумерный массив символов (тип char [,]).
 //Создать строку из символов этого массива.
 
-char [,] matrixChar = new char [,] 
+char[,] matrixChar = new char[,]
 {
     {'a', 'b', 'c', 'd'},
     {'e', 'f', 'g', 'h'},
     {'i', 'j', 'k', 'l'}
 };
 
-string str = "";
-for (int i = 0; i < matrixChar.GetLength(0); i++)
+string JoinChar(char[,] matrixChar, string str)
 {
-    for (int j = 0; j < matrixChar.GetLength(1); j++)
+    for (int i = 0; i < matrixChar.GetLength(0); i++)
     {
-        str += matrixChar[i, j]; 
+        for (int j = 0; j < matrixChar.GetLength(1); j++)
+        {
+            str += matrixChar[i, j];
+        }
     }
+    return str;
 }
-Console.WriteLine(str);
+
+string str = "";
+string newStr = JoinChar(matrixChar, str);
+Console.WriteLine(newStr);
