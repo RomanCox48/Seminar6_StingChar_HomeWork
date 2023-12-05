3b55bbdf669d48c29b46edada7aa4073391efa0c
RomanKosov <kosovr48@gmail.com>
2023-12-05 23:58:56 +0500

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задайте строку, состоящую из слов, разделенных пробелами.
+//Сформировать строку, в которой слова расположены в обратном порядке.
+//В полученной строке слова должны быть также разделены пробелами.
+
+Console.WriteLine("Введите строку, состоящую из слов, разделенных пробелами:");
+string[] sentence = Console.ReadLine().Split(" ").ToArray();
+for (int i = 0; i < sentence.Length/2; i++)
+{
+    string temp = "";
+    temp = sentence [i];
+    sentence [i] = sentence [sentence.Length -1-i];
+    sentence [sentence.Length -1-i] = temp;
+}
+Console.WriteLine($"{string.Join(" ", sentence)}");
+
