d0a4deaf98db4f169811b4808d31352683adb0f3
RomanKosov <kosovr48@gmail.com>
2023-12-05 22:11:22 +0500

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задайте двумерный массив символов (тип char [,]).
+//Создать строку из символов этого массива.
+
+char [,] matrixChar = new char [,] 
+{
+    {'a', 'b', 'c', 'd'},
+    {'e', 'f', 'g', 'h'},
+    {'i', 'j', 'k', 'l'}
+};
+
+string str = "";
+for (int i = 0; i < matrixChar.GetLength(0); i++)
+{
+    for (int j = 0; j < matrixChar.GetLength(1); j++)
+    {
+        str += matrixChar[i, j]; 
+    }
+}
+Console.WriteLine(str);
